48eb2f8232b232ecaa5973c508e7bc9e6226de5d
RoiMartinezRoque <r.martinezr.2023@alumnos.urjc.es>
2025-02-26 20:47:52 +0100

    Eliminar todos los proyectos asociados a un grupo, cuando se elimina este

--- src/main/java/com/group12/taskmanager/services/GroupService.java
+++ src/main/java/com/group12/taskmanager/services/GroupService.java
@@ -1,6 +1,7 @@
 package com.group12.taskmanager.services;
 
 import com.group12.taskmanager.models.Group;
+import com.group12.taskmanager.models.Project;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -45,6 +46,11 @@ public class GroupService {
     public boolean removeGroup(int groupId, String userName) {
         Group group = findGroupById(groupId);
         if (group != null && !group.getName().equals("USER_"+userName)) {
+            // Eliminar todos los proyectos asociados al grupo
+            List<Project> projectsToRemove = group.getProjects();
+            for (Project project : projectsToRemove) {
+                ProjectService.getInstance().removeProject(project.getId());
+            }
             return GROUPS.removeIf(g -> g.getId() == groupId);
         }
         return false;
